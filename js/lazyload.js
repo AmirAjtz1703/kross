7aae2b9ddab0c9d4e81998a3b0e2ca9826d0c3ea
AmirAjtz1703 <ncp94324@bcaoo.com>
2019-06-21 20:41:07 +0600

    last --v

--- js/lazyload.js
+++ js/lazyload.js
@@ -37,11 +37,7 @@
 
 
 
-document.body.onload = function () {
-  setTimeout(function () {
-    var preloader = document.getElementsByClassName('loader');
-    if(!preloader.classList.contains('done')) {
-      preloader.classList.add('done')
-    }
-  }, 1000);
-}
+$(window).on("load", function() {
+  $(".load-icon").fadeOut();
+  $(".pre-loader").delay(1000).fadeOut("slow");
+});
